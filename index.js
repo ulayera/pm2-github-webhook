ef03ba8c724eaf3b9822443facf3705415abb480
Ulises Layera <ulayera@gmail.com>
2018-12-12 20:42:45 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+const express = require("express");
+const bodyParser = require('body-parser')
+const fs = require('fs');
+const { exec } = require('child_process');
+let app = express();
+let port = process.env.PORT | 9001;
+let cwd = '/home/ulayera/';
+
+app.use(bodyParser.json());
+
+app.post("/webhooks/github", function (req, res) {
+    let event = req.headers['x-github-event'];
+    console.log(new Date() + ' - Hook type: ' + event);
+    if ('ping' === event) {
+        res.sendStatus(200);
+    } else if ('push' === event) {
+        let repo = req.body.repository.name;
+        let branch = req.body.ref.replace('refs/heads/', '');
+	let command = `pm2 deploy ${cwd}${repo}.config.js ${branch}`;
+        console.log('Command to execute: ' + command);
+	let config;
+        try {
+            config = require(cwd + repo + '.config');
+        } catch (e) {}
+	if (config && config.apps[0].name === repo && config.deploy[branch]) {
+            console.log('Command is supported');
+            exec(command, (err, stdout, stderr) => {
+                var dt = new Date();
+                let date = dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate();
+                let path = cwd+'logs/'+repo+'/'+branch+'/';
+                if (err) {
+                    fs.writeFile(path+date+'-err.log', err, (err) => {});
+                    console.error('Error: see ' + path+date+'-err.log');
+                } else {
+                    console.log('Success: see ' + path+date+'-err.log');
+                }
+                if (stdout) {
+                    fs.writeFile(path+date+'-stdout.log', stdout, (err) => {});
+                }
+                if (stderr) {
+                    fs.writeFile(path+date+'-stderr.log', stderr, (err) => {});
+                }
+            });
+        }
+        res.sendStatus(200);
+    } else {
+         res.status(501).send('Hook type not implemented');
+    }
+})
+
+app.listen(port, function () {
+  console.log('pm2-github-webhook listening on port ' + port + '!');
+});
